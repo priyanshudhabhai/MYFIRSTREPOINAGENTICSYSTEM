efc80d913fe3923101666c2fd455268304b94b0c
priyanshudhabhai <dhabhaipriyanshu8@gmail.com>
2026-02-05 12:55:33 +0530

    Create question3.py

---
+++ question3.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age_input = input("Enter your age: ")
+age = int(age_input)
+is_active_input = input("Are you an active user? (True/False): ")
+is_active = is_active_input == "True"
+print(f"User {name} is {age] years old. Active status: {is_active}")
