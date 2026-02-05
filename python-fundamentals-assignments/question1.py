eed14c159b25a2ec66e43c1a29ca46ec52d8f040
priyanshudhabhai <dhabhaipriyanshu8@gmail.com>
2026-02-05 12:30:20 +0530

    Update question1.py

--- python-fundamentals-assignments/question1.py
+++ python-fundamentals-assignments/question1.py
@@ -2,4 +2,4 @@ birth_year_input = input("Enter your birth year: ")
 birth_year = int(birth_year_input)
 current_year = 2026
 age = current_year - birth_year
-print(age)
+print(f"You are {age} years old.")
