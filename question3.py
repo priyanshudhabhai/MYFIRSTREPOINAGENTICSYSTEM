91f22dc6644c71d1ace0450e4a0152065ed20ac3
priyanshu <dhabhaipriyanshu8@gmail.com>
2026-02-05 13:46:07 +0530

    Fixed syntax errors and indentation in all questions

--- question3.py
+++ question3.py
@@ -3,4 +3,4 @@ age_input = input("Enter your age: ")
 age = int(age_input)
 is_active_input = input("Are you an active user? (True/False): ")
 is_active = is_active_input == "True"
-print(f"User {name} is {age] years old. Active status: {is_active}")
+print(f"User {name} is {age} years old. Active status: {is_active}")
