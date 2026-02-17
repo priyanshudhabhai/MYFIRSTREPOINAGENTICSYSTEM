50862a0832de59d04ff9328eadaf1cabd4bc2738
priyanshu <dhabhaipriyanshu8@gmail.com>
2026-02-17 15:58:38 +0530

    all changes saved

--- question2.py
+++ question2.py
@@ -3,6 +3,7 @@ if accuracy_input.replace('.', '', 1).isdigit():
     accuracy = float(accuracy_input)
     print(f"Model accuracy is {accuracy}")
 else:
+    
     print("Please enter a numeric value.")
 
 
